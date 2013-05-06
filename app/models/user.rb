4527c9c55c00abf5de1bd07bf921d49953453cec
Amerson Lin <amerson@turnary.com>
2013-05-06 15:58:01 +0800

    Added name to user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
          :token_authenticatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
 
   has_and_belongs_to_many :teams
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130502135438) do
+ActiveRecord::Schema.define(:version => 20130506074646) do
 
   create_table "broadcasts", :force => true do |t|
     t.string   "description"
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(:version => 20130502135438) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "authentication_token"
+    t.string   "name"
   end
 
   add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
